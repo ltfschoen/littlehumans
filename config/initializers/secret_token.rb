0c978628d4793e36ec5ba67b4666291792d7740f
ltfschoen <ltfschoen@gmail.com>
2014-03-21 22:17:02 +1100

    Created App and Database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LittlehumansApp::Application.config.secret_key_base = 'bf357e12fde512c88b89b6b225a545bafafa9f90adfeacc45b752e8ef7c68fd1e717d8383de2bbdfb466bfc507205632dd0825eb5e6515dd530e74e10b270066'
