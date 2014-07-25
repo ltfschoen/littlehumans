a3d8112d25aee3a224c377c8ea29aab1457a9865
ltfschoen <ltfschoen@gmail.com>
2014-07-25 20:01:39 +1000

    Commited files to allow merge from branch 6_style

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   end
 
   if Rails.env == 'production' 
-	  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   end
 
   if Rails.env == 'development' 
@@ -27,7 +27,7 @@ Devise.setup do |config|
   if Rails.env == 'production' 
     config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], { access_type: "offline", approval_prompt: "" }
   end
-  	
+    
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
