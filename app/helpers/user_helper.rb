fcc1e02f7e4038ea02d64eb5f9447e2fe62ede99
ltfschoen <ltfschoen@gmail.com>
2014-07-26 22:09:23 +1000

    Further Refactored User Model into the Helper Method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,24 +1,35 @@
 module UserHelper
   module OAuth
     # check if user exists in database with provider and uid matching oauth parameter
-    def self.client_check_provider_and_uid(auth)
+    def self.client_check_provider_uid_email(auth)
       # grab user matching oauth parameter provider and user id from database
-      user = User.where(:provider => auth.provider, :uid => auth.uid).first
+      existing_user = User.where(:provider => auth.provider, :uid => auth.uid).first
 
       # no further action if match found
-      if user
-        return user
+      if existing_user
+        return existing_user
+      else
+        # check if user exists in database with email matching oauth parameter
+        if auth.provider == "facebook"
+          existing_user = User.where(:email => auth.info.email).first
+        elsif auth.provider == "twitter"
+          existing_user = User.where(:email => auth.uid + "@twitter.com").first
+        elsif auth.provider == "google_oauth2"
+          existing_user = User.where(:email => auth.info.email).first
+        end
       end
-    end
 
-    # check if user exists in database with email matching oauth parameter
-    def self.client_check_email(auth)
-      # check for match between oauth parameter email and emails in the database
-      registered_user = User.where(:email => auth.uid + "@twitter.com").first
-
-      # no further action if match found as user is already registered
-      if registered_user
-        return registered_user
+      # no action if match found as user is already registered. otherwise register them
+      if existing_user
+        return existing_user
+      else
+        if auth.provider == "facebook"
+          new_user = UserHelper::OAuth::client_facebook_register_user(auth)
+        elsif auth.provider == "twitter"
+          new_user = UserHelper::OAuth::client_twitter_register_user(auth)
+        elsif auth.provider == "google_oauth2"
+          new_user = UserHelper::OAuth::client_google_register_user(auth)
+        end
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,49 +6,17 @@ class User < ActiveRecord::Base
 
   has_many :rosters
   has_many :deliveries
-
+  
+  # check if oauth matches an existing user otherwise register them
   def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
-    user = UserHelper::OAuth::client_check_provider_and_uid(auth)
-
-    # check for match between oauth parameter email and emails in the database
-    registered_user = User.where(:email => auth.info.email).first
-
-    # no further action if match found as user is already registered
-    if registered_user
-      return registered_user
-    # register the oauth parameters in database if not already registered 
-    else
-      user = UserHelper::OAuth::client_facebook_register_user(auth)
-    end 
+    user = UserHelper::OAuth::client_check_provider_uid_email(auth)
   end
 
   def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
-    user = UserHelper::OAuth::client_check_provider_and_uid(auth)
-
-    # check for match between oauth parameter email and emails in the database
-    registered_user = User.where(:email => auth.uid + "@twitter.com").first
-
-    # no further action if match found as user is already registered
-    if registered_user
-      return registered_user
-    # register the oauth parameters in database if not already registered 
-    else
-      user = UserHelper::OAuth::client_twitter_register_user(auth)
-    end
+    user = UserHelper::OAuth::client_check_provider_uid_email(auth)
   end
 
   def self.find_for_google_oauth2(auth, signed_in_resource = nil)
-    user = UserHelper::OAuth::client_check_provider_and_uid(auth)
-
-    # check for match between oauth parameter email and emails in the database
-    registered_user = User.where(:email => auth.info.email).first
-
-    # no further action if match found as user is already registered
-    if registered_user
-      return registered_user
-    # register the oauth parameters in database if not already registered 
-    else
-      user = UserHelper::OAuth::client_google_register_user(auth)
-    end
+    user = UserHelper::OAuth::client_check_provider_uid_email(auth)
   end
 end
